69b40f4c138b47599c6cdfacefbc3b472741fbcc
ajayraj201651006 <ajayraj003016@gmail.com>
2019-01-06 16:46:05 +0530

    update

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,13 +3,13 @@ import Aux from '../../hoc/Auxes';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
-	
+
     state = {
         	ingredients: {
-        		salad: 1,
-        		cheese: 2,
-        		bacon: 1,
-        		meat: 2
+        		salad: 0,
+        		bacon: 0,
+        		cheese: 0,
+        		meat: 0
         	}
         }
 
