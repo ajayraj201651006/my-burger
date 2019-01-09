68712fd610fffcae01fd654b6f387ea0e2e95030
ajayraj201651006 <ajayraj003016@gmail.com>
2019-01-09 19:00:15 +0530

    update

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './BuildControl.css';
+
+const buildcontrol = (props) => (
+         <div>
+
+         </div>
+	);
+
+export default buildcontrol
---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const buildcontrols = (props) => (
+        <div>
+
+        </div>
+	);
+
+export default buildcontrols
