eb407078660b749afeb74a9f65ecdf6abfdf5919
ajayraj201651006 <ajayraj003016@gmail.com>
2019-01-13 19:58:26 +0530

    update

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,11 +6,15 @@ import Aux from '../../../hoc/Auxes';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sidedrawer = (props) => {
+	let attachedClasses = [classes.SideDrawer, classes.Close];
+	if(props.open){
+		attachedClasses = [classes.SideDrawer, classes.Open];
+	}
     
     return(
     	    <Aux>
-    	        <Backdrop show/>
-	            <div className={classes.SideDrawer}>
+    	        <Backdrop show={props.open} clicked={props.closed}/>
+	            <div className={attachedClasses.join(' ')}>
 	              <div className={classes.Logo}>
 	                <Logo />
 	              </div>
