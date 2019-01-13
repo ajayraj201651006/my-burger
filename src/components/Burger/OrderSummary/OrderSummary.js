f6fba238d4cda3f83e82e4ca15a299c96afa9e91
ajayraj201651006 <ajayraj003016@gmail.com>
2019-01-14 00:41:27 +0530

    update

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,7 +4,7 @@ import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
     componentWillUpdate() {
-        console.log("[OrderSummary] will Update")
+        console.log("[OrderSummary] will Update");
     }
 
     render() {
