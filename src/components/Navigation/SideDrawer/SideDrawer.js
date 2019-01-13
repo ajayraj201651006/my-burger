10a0a8c729e6cf0dc2c355624458d2566d0156aa
ajayraj201651006 <ajayraj003016@gmail.com>
2019-01-13 19:34:04 +0530

    update

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,23 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Aux from '../../../hoc/Auxes';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sidedrawer = (props) => {
     
     return(
-            <div className={classes.SideDrawer}>
-              <div className={classes.Logo}>
-                <Logo />
-              </div>
-               <nav>
-                  <NavigationItems />
-               </nav>
-            </div>
+    	    <Aux>
+    	        <Backdrop show/>
+	            <div className={classes.SideDrawer}>
+	              <div className={classes.Logo}>
+	                <Logo />
+	              </div>
+	               <nav>
+	                  <NavigationItems />
+	               </nav>
+	            </div>
+            </Aux>
     	);
 };
 
