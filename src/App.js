6bd727e30a2cb41abeebbac89b52b0b246aa8b6a
ajayraj201651006 <ajayraj003016@gmail.com>
2019-02-19 15:12:49 +0530

    implementing links

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders'''
+import Orders from './containers/Orders/Orders';
 import {Route, Switch} from 'react-router-dom';
 
 class App extends Component {
--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,10 +2,12 @@ import React from 'react';
 import classes from './Order.css';
 
 const order = (props) => {
+    return (
     <div className={classes.Order}>
         <p>Ingredients: salad(1)</p>
         <p>Price: <strong>USD 5.00</strong></p>
     </div>
+    );
 }
 
 export default order;
