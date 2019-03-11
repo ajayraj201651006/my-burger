cc30048d362a908a6d87cf1c92ad61773d3b0d7d
ajayraj201651006 <ajayraj003016@gmail.com>
2019-03-11 16:58:35 +0530

    create custom input

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './ContactData.css';
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 import axios from '../../../axios-orders';
 
 class ContactData extends Component {
@@ -45,10 +46,10 @@ class ContactData extends Component {
     render() {
         let form = (
             <form>
-                   <input className={classes.Input} type="text" name="Name" placeholder="Your Name" />
-                   <input className={classes.Input} type="email" email="Email" placeholder="Your Email" />
-                   <input className={classes.Input} type="text" street="Street" placeholder="Street" />
-                   <input className={classes.Input} type="text" postal="Postal" placeholder="Postal Code" />
+                   <Input inputtype="input" type="text" name="Name" placeholder="Your Name" />
+                   <Input inputtype="input" type="email" email="Email" placeholder="Your Email" />
+                   <Input inputtype="input" type="text" street="Street" placeholder="Street" />
+                   <Input inputtype="input"  type="text" postal="Postal" placeholder="Postal Code" />
                    <Button btnType="Success" clicked={this.orderHandler}>Order Now</Button>   
                </form>
         );
